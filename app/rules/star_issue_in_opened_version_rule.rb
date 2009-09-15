3bca6f536937386f44df5a0dfb4cc51b0c64619f
bruno <bruno@bruno-laptop>
2009-09-15 19:27:11 -0300

    regra abertura de tarefa em iteração em andamento

---
+++ app/rules/star_issue_in_opened_version_rule.rb
@@ -0,0 +1,11 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class StarIssueInOpenedVersionRule
+ def valid?(issue)
+    started = Issue.count(:conditions=>["id=? and status_id=?",issue.id,issue.status_id])==0 && IssueStatus.count(:conditions=>["id=? and name=?",issue.status_id,"Em Andamento"])>0
+    version = Version.find_by_id(issue.fixed_version_id)
+    return true unless started
+    return !version.nil? &&  !version.completed?
+  end
+end
