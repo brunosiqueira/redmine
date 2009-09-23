8029a1e3fbdd235cd1edd3c7c6920fc589418c52
Bruno <bruno@bruno-laptop.(none)>
2009-09-23 19:23:21 -0300

    criando administração de soluções 	modified:   app/models/solution.rb 	modified:   app/views/admin/index.rhtml 	new file:   app/views/solutions/index.html.erb 	new file:   app/views/solutions/new.html.erb 	new file:   app/views/solutions/show.html.erb 	modified:   config/locales/bs.yml 	modified:   config/locales/ca.yml 	modified:   config/locales/cs.yml 	modified:   config/locales/da.yml 	modified:   config/locales/de.yml 	modified:   config/locales/en.yml 	modified:   config/locales/es.yml 	modified:   config/locales/fi.yml 	modified:   config/locales/fr.yml 	modified:   config/locales/gl.yml 	modified:   config/locales/he.yml 	modified:   config/locales/hu.yml 	modified:   config/locales/it.yml 	modified:   config/locales/ja.yml 	modified:   config/locales/ko.yml 	modified:   config/locales/lt.yml 	modified:   config/locales/nl.yml 	modified:   config/locales/no.yml 	modified:   config/locales/pl.yml 	modified:   config/locales/pt-BR.yml 	modified:   config/locales/pt.yml 	modified:   config/locales/ro.yml 	modified:   config/locales/ru.yml 	modified:   config/locales/sk.yml 	modified:   config/locales/sr.yml 	modified:   config/locales/sv.yml 	modified:   config/locales/th.yml 	modified:   config/locales/tr.yml 	modified:   config/locales/uk.yml 	modified:   config/locales/vi.yml 	modified:   config/locales/zh-TW.yml 	modified:   config/locales/zh.yml 	modified:   config/routes.rb 	modified:   test/fixtures/solutions.yml 	deleted:    test/functional/rules_controller_test.rb 	modified:   config/locales/bg.yml 	modified:   config/locales/sl.yml 	app/controllers/solutions_controller.rb 	app/helpers/solutions_helper.rb 	app/rules/start_issue_in_opened_version_rule.rb 	app/views/issues/_pre_requirement.html.erb 	app/views/layouts/solutions.html.erb 	app/views/rules/edit.html.erb 	app/views/rules/index.html.erb 	app/views/rules/list.html.erb 	app/views/rules/show.html.erb 	app/views/solutions/edit.html.erb 	test/functional/rules_controller_test.rb 	test/functional/solutions_controller_test.rb 	test/unit/rules/solutions/solution_move_issue_to_finished_version_and_close_test.rb 	test/unit/rules/solutions/solution_move_issue_to_open_version_test.rb 	test/unit/rules/start_issue_in_opened_version_rule_test.rb

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -5,4 +5,17 @@ class Solution < ActiveRecord::Base
   def to_s
     message
   end
+
+  def self.new_solutions
+    files = []
+    path = "app/rules/solutions/"
+    Dir.glob("#{path}*.rb").each do |f|
+      name = f[path.length,f.length-3-path.length].camelize
+      if Solution.count(:conditions=>{:name=>name}) == 0
+        files << name
+      end
+    end
+    files
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :solutions
+
   map.resources :rules
 
   # Add your own custom routes here.
